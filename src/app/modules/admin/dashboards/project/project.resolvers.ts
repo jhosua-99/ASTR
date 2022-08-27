6f4c0a8ca2b09053518cd41319d3fe842369abcc
Santiago Albornoz Tibasoza <salbornozt@unbosque.edu.co>
2022-08-27 17:03:14 -0500

    pannel data

--- src/app/modules/admin/dashboards/project/project.resolvers.ts
+++ src/app/modules/admin/dashboards/project/project.resolvers.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 import { ProjectService } from 'app/modules/admin/dashboards/project/project.service';
+import { ProcesoService } from 'app/services/processs/proceso.service';
 
 @Injectable({
     providedIn: 'root'
@@ -28,5 +29,61 @@ export class ProjectResolver implements Resolve<any>
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any>
     {
         return this._projectService.getData();
+    }
+}
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ProcessResolver implements Resolve<any>
+{
+    /**
+     * Constructor
+     */
+    constructor(private _procesoService:ProcesoService)
+    {
+    }
+
+    // -----------------------------------------------------------------------------------------------------
+    // @ Public methods
+    // -----------------------------------------------------------------------------------------------------
+
+    /**
+     * Resolver
+     *
+     * @param route
+     * @param state
+     */
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any>
+    {
+        return this._procesoService.getProcesosPorRenovar();
+    }
+}
+
+@Injectable({
+    providedIn: 'root'
+})
+export class PanelResolver implements Resolve<any>
+{
+    /**
+     * Constructor
+     */
+    constructor(private _procesoService:ProcesoService)
+    {
+    }
+
+    // -----------------------------------------------------------------------------------------------------
+    // @ Public methods
+    // -----------------------------------------------------------------------------------------------------
+
+    /**
+     * Resolver
+     *
+     * @param route
+     * @param state
+     */
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any>
+    {
+        return this._procesoService.getPanelData();
     }
 }
